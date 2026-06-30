6f1df940e83b1cec1d6cdb3a774ef23323c4652d
Jerome Eteve <jeteve@users.noreply.github.com>
2026-06-30 20:33:33 +0000

    Better leaker code

--- tests/c/leaker.c
+++ tests/c/leaker.c
@@ -1,8 +1,15 @@
 // A minimal C program that leaks memory.
 #include <stdio.h>
 #include <stdlib.h>
+#include <strings.h>
 
 int main(void) {
-    void *leaked_memory = malloc(1024);
+char *leaked = malloc(1024 * sizeof(char));
+
+    strcpy(leaked, "This memory will be leaked");
+    printf("%s\n", leaked);
+
+    leaked = NULL;
+
     return 0;
 }
